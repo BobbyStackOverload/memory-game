ccfbf509946f37a7504ba9cd36c6197c1656c549
Bobby W Bryant <sirbob1999@gmail.com>
2020-03-24 15:23:09 -0400

    worked on logo

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -5,6 +5,7 @@ class MemoryCard extends Component {
   render() {
     return (
       <div className="MemoryCard">
+          <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt=""></img>
       </div>
     );
   }
