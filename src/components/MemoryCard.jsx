2253f9a2e142b5fb1d834acd832656adc3da5021
Bobby W Bryant <sirbob1999@gmail.com>
2020-03-25 12:10:08 -0400

    made game clickable

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,11 +2,33 @@ import React, { Component } from "react";
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
+  state = {
+    isFlipped: false
+  };
+  
+  clickHandler = () => {
+    this.setState({ 
+      isFlipped: !this.state.isFlipped 
+    });
+  };
+
+ 
   render() {
+    const { isFlipped } = this.state;
+    
+    const MemoryCardInnerClass = 
+      'MemoryCardInner ' + (isFlipped === true && 'flipped');
+  
     return (
-      <div className="MemoryCard">
+      <div className="MemoryCard" onClick={this.clickHandler}>
+        <div className={MemoryCardInnerClass}>
+          <div className="MemoryCardBack">
           <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt=""></img>
-      </div>
+          </div>
+          <div className="MemoryCardFront"><p>∆</p>
+          </div>
+        </div>
+        </div>
     );
   }
 }
